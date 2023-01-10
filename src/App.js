3a9450d775c095ab9985deb4e23843a0907f5230
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-01-10 17:48:00 +0100

    ✨ creation des pages + logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/pages/About";
+import ErrorPage from "./components/pages/ErrorPage";
+import Home from "./components/pages/Home";
+import Logements from "./components/pages/Logements";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/logements" element={<Logements />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../reusable-ui/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  );
+}
