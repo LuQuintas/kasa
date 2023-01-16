2fc94848151112b734ddc08c620ec5d7264b0725
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-01-16 21:31:47 +0100

    ♻️ F01 : refacto code navbar

--- src/components/reusable-ui/Navbar/NavBar.js
+++ src/components/reusable-ui/Navbar/NavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import Logo from "../Logo";
 import NavBarRightSide from "./NavBarRightSide";
 
 export default function NavBar() {
   return (
     <NavBarStyled>
-      <Logo />
+      <img src="/images/logo.png" alt="Logo de kasa" />
       <NavBarRightSide />
     </NavBarStyled>
   );
@@ -16,4 +15,7 @@ const NavBarStyled = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 10px;
+  img {
+    width: 45%;
+  }
 `;
