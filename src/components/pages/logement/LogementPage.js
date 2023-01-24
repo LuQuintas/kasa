fadd07bd12c9ba958ebe7c1a47aac6a251625f7a
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-01-24 18:36:17 +0100

    ✨ F02 : Home> LogementPage> create Tag.js

--- src/components/pages/logement/LogementPage.js
+++ src/components/pages/logement/LogementPage.js
@@ -1,24 +1,37 @@
 import styled from "styled-components";
 import NavBar from "../../reusable-ui/NavBar";
 import Footer from "../../reusable-ui/Footer";
-
+import { useParams } from "react-router-dom";
+import datasProducts from "../../../assets//Datas/Api.json";
+import Tag from "./Tag";
 export default function Logement() {
+  const idProduct = useParams();
+
+  const product = datasProducts.find((product) => product.id === idProduct.id);
+
   return (
     <LogementStyled>
       <NavBar />
       <div className="container">
-        <img src="{image}" alt="" />
+        <img src={product.pictures} alt={product.title} />
         <div className="title-host">
-          <div>title</div>
-          <div>info</div>
+          <div>
+            <h1>{product.title}</h1>
+            <span className="location">{product.location}</span>
+          </div>
+          <div>{product.host.name}</div>
         </div>
-        <div className="tag-rating">
-          <div>tag</div>
-          <div> rating</div>
+        <div className="tags">
+          {product.tags.map((tags) => (
+            <Tag name={tags} />
+          ))}
+          <div className="rating">
+            <span>{product.rating}</span>
+          </div>
         </div>
         <div className="description">
-          <div>description</div>
-          <div>equipement</div>
+          <div>{product.description}</div>
+          <div>{product.equipments}</div>
         </div>
       </div>
 
@@ -32,21 +45,53 @@ const LogementStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   margin: auto;
-  max-width: 1440px;
+  max-width: 1240px;
   .container {
-    background: red;
+    /* background: red; */
     display: flex;
     flex-direction: column;
     justify-content: center;
     width: 100%;
     align-items: center;
+    img {
+      width: 1240px;
+      /* background: red; */
+      height: 415px;
+    }
+    h1 {
+      font-size: 36px;
+      font-weight: 500;
+      color: #ff6060;
+    }
+    .location {
+      color: #ff6060;
+      font-size: 18px;
+      margin-bottom: 23px;
+    }
   }
   .title-host,
   .tag-rating,
   .description {
     display: flex;
-    background: blue;
+    color: white;
+    /* background: blue; */
     width: 100%;
+    min-height: 50px;
     justify-content: space-between;
+    margin-top: 15px;
+    padding: 5px;
+  }
+
+  .tags {
+    width: 100%;
+    display: flex;
+    margin-top: 15px;
+    /* justify-items: self-end; */
+    .rating {
+      text-align: right;
+      /* justify-items: center; */
+      width: 100%;
+    }
+    /* justify-content: space-around; */
   }
 `;
---
+++ src/components/pages/logement/Tag.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default function Tag({ name }) {
+  return <TagStyled>{name}</TagStyled>;
+}
+const TagStyled = styled.div`
+  display: flex;
+  min-width: 115px;
+  height: 25px;
+  border-radius: 10px;
+  background-color: #ff6060;
+  color: white;
+  font-size: 14px;
+  margin-right: 10px;
+  justify-content: center;
+  padding: 5px 15px;
+  align-items: center;
+`;
