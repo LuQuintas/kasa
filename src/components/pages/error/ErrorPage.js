368850684f282474ecbef675ee0cd08758960f56
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-02-02 18:01:37 +0100

    ✨ F02: APP> ROUTE> Created ErroPage component

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,7 +1,43 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Footer from "../../reusable-ui/Footer";
+import NavBar from "../../reusable-ui/NavBar";
+
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>ErrorPage</h1>
-    </div>
+    <ErrorStyled>
+      <NavBar />
+      <div className="title_error">
+        <h1>404</h1>
+        <span>"Oups ! La page que vous demandez n'existe pas ."</span>
+        <NavLink to="/"> Retourner sur la page d'accueil</NavLink>
+      </div>
+      <Footer />
+
+      {/* <Footer /> */}
+    </ErrorStyled>
   );
 }
+const ErrorStyled = styled.div`
+  text-align: center;
+  .title_error {
+    margin-top: 200px;
+    h1 {
+      color: #ff6060;
+      font-size: 96px;
+      font-weight: 700;
+    }
+    span {
+      line-height: 25px;
+      text-align: center;
+      font-size: 18px;
+      color: #ff6060;
+    }
+    a {
+      margin: 130px auto;
+      display: block;
+      color: #ff6060;
+      font-size: 14px;
+    }
+  }
+`;
