01d3badd57c92dc08ddadad3027551ff29ed42bb
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-02-07 20:21:04 +0100

    ✨ F03: LogementPage> create component carousel.js with usestate for slider

--- src/components/pages/logement/Carousel.js
+++ src/components/pages/logement/Carousel.js
@@ -1,29 +1,29 @@
 import { useState } from "react";
 import styled from "styled-components";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
-export default function Carousel({ img, alt, imageSlider }) {
+export default function Carousel({ slides }) {
   const [currImg, setCurrImg] = useState(0);
-  console.log(currImg);
 
   const prevSlide = () => {
-    setCurrImg(currImg - 1);
-    if (currImg === 0) setCurrImg(imageSlider.length - 1);
+    const firstSlide = currImg === 0;
+    const newImage = firstSlide ? slides.length - 1 : currImg - 1;
+    setCurrImg(newImage);
   };
   const nextSlide = () => {
-    setCurrImg(currImg + 1);
-    if (currImg === imageSlider.length - 1) setCurrImg(0);
+    const lastSlide = currImg === slides.length - 1;
+    const newImage = lastSlide ? 0 : currImg + 1;
+    setCurrImg(newImage);
   };
   return (
     <CarouselStyled>
       <div className="carouselInner">
         <div className="left" onClick={prevSlide}>
-          <ArrowBackIosIcon className="arrow" />
+          <ArrowForwardIosIcon className="arrow_left" />
         </div>
-        <img src={img[currImg]} alt={alt} />
+        <img src={slides[currImg]} alt="" />
         <div className="right" onClick={nextSlide}>
-          <ArrowForwardIosIcon className="arrow" />
+          <ArrowForwardIosIcon className="arrow_right" />
         </div>
       </div>
     </CarouselStyled>
@@ -31,29 +31,46 @@ export default function Carousel({ img, alt, imageSlider }) {
 }
 const CarouselStyled = styled.div`
   height: 255px;
-  border-radius: 10px;
-  /* background: black; */
+  max-width: 1240px;
 
   min-width: 335px;
   margin: 20px auto;
   .carouselInner {
+    position: relative;
+    justify-content: space-between;
     display: flex;
     height: 100%;
-    width: 100%;
+    margin: 20px;
+
     img {
+      border-radius: 10px;
+      position: absolute;
+      z-index: 0;
+      width: 100%;
+      height: 100%;
       object-fit: cover;
     }
-    .arrow {
-      color: black;
+    .arrow_left {
+      position: absolute;
+      color: white;
+      top: 45%;
+      left: 5%;
+      transform: rotate(180deg);
+    }
+    .arrow_right {
+      position: absolute;
+      top: 45%;
+      color: white;
+      right: 5%;
     }
     .left,
     .right {
+      position: relative;
+      z-index: 1;
       display: flex;
-      justify-content: center;
-      align-items: center;
-      background: blue;
       height: 100%;
-      width: 15%;
+      width: 40px;
+      cursor: pointer;
     }
   }
 `;
