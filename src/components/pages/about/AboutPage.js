1124d9c8aabbf4a66593ef3d87e2e2d621db9d70
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-02-07 20:20:00 +0100

    ♻️ F03: AboutPage> refecto map component collapse

--- src/components/pages/about/AboutPage.js
+++ src/components/pages/about/AboutPage.js
@@ -4,29 +4,16 @@ import Footer from "../../reusable-ui/Footer";
 import NavBar from "../../reusable-ui/NavBar";
 import bannerAbout from "../about/images/bannerAbout.png";
 import styled from "styled-components";
+import infoAbout from "../../../assets/Datas/infoAbout.json";
 
 export default function About() {
   return (
     <AboutStyled>
       <NavBar />
       <Banner ClassName="banner" image={bannerAbout} />
-      <Collapse
-        title="Fiabilité"
-        texte="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-      />
-      <Collapse
-        title="Respect"
-        texte="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Service"
-        texte="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-      />
-      <Collapse
-        title="Sécurité"
-        texte="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      />
-
+      {infoAbout.map((info) => (
+        <Collapse title={info.title} texte={info.description} />
+      ))}
       <Footer />
     </AboutStyled>
   );
@@ -37,6 +24,7 @@ const AboutStyled = styled.div`
     height: 223px;
     max-width: 1240px;
     @media screen and (min-width: 1024px) {
+      margin-bottom: 30px;
       img,
       .bg-dark {
         border-radius: 25px;
