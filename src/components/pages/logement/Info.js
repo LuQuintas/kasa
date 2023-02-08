5698702b86bef9445a87777c695a19d0e23b7219
quintasludovic <quintas.dev.ludovic@gmail.com>
2023-02-08 18:50:52 +0100

    🚚 F03: rename files component

---
+++ src/components/pages/logement/Info.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export default function Info({ title, location }) {
+  return (
+    <HostStyled>
+      <h2>{title}</h2>
+      <span>{location}</span>
+    </HostStyled>
+  );
+}
+const HostStyled = styled.div`
+  width: 100%;
+  color: #ff6060;
+  letter-spacing: 1px;
+  line-height: 140%;
+  margin-bottom: 10px;
+  h2 {
+    font-weight: 500;
+    font-size: 18px;
+    margin-bottom: 5px;
+  }
+  span {
+    font-size: 14px;
+  }
+`;
